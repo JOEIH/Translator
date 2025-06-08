ca86828ab896db0a175e23ec8e16927f043ca1bd
JOEIH <joh73459@gmail.com>
2025-06-09 05:37:55 +0900

    ✨ Feat : 번역 결과 출력

--- client/src/js/selectText.js
+++ client/src/js/selectText.js
@@ -2,17 +2,18 @@ const confirm_button = document.createElement('button');
 
 // 텍스트를 드래그하면 번역하기 버튼 생성
 document.addEventListener('mouseup', function (e) {
-  // mouseup 이벤트가 발생하는 요소를 텍스트가 들어 있는 p태그로 한정
-  if (e.target.tagName != 'P') return;
+  if (e.target.tagName == 'BUTTON') return;
 
   const selection = document.getSelection();
   const selectedText = selection.toString().trim();
 
   if (!selectedText) return;
 
-  const base_tag = selection.baseNode.parentNode;
+  // focusNode로 선택한 영역의 가장 끝에 있는 노드 가져오기
+  // const base_tag = selection.baseNode.parentNode;
+  const base_tag = selection.focusNode.parentNode;
 
-  if (!base_tag) return;
+  if (!base_tag.classList.contains('extracted')) return;
 
   // 이전 버튼 요소들 중첩되지 않게 삭제처리
   const buttons = document.querySelectorAll('.button-translate');
@@ -31,7 +32,9 @@ document.addEventListener('mouseup', function (e) {
   // 번역하기 버튼 누르면 api 요청 전송
   confirm_button.onclick = () => {
     if (send_text) {
-      onClickButton(send_text);
+      onClickButton(send_text, base_tag);
+
+      confirm_button.remove();
     }
   };
 });
@@ -48,6 +51,13 @@ async function getTranslatedResult(url = '', text) {
       },
     });
 
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      console.log('error: ', errorMessage);
+      alert('번역 실패');
+      return;
+    }
+
     const data = await response.json();
 
     return data.result;
@@ -55,11 +65,52 @@ async function getTranslatedResult(url = '', text) {
 }
 
 // 번역하기 버튼 클릭할 때 요청 전송
-async function onClickButton(selectedText) {
+async function onClickButton(selectedText, baseTag) {
   const result = await getTranslatedResult(
     `${import.meta.env.VITE_SERVER_URL}`,
     selectedText
   );
 
-  console.log(result);
+  const div = showResult(result.translations);
+
+  if (div) {
+    baseTag.appendChild(div);
+  }
+}
+
+// 번역 결과창
+function showResult(result) {
+  if (!result) {
+    alert('번역 실패');
+    return;
+  }
+
+  // 이전 번역 결과 중첩되지 않게 삭제처리
+  const translated = document.querySelectorAll('.translated');
+  if (translated) {
+    translated.forEach((item) => {
+      item.remove();
+    });
+  }
+
+  const div = document.createElement('div');
+
+  div.setAttribute('class', 'translated');
+
+  for (let i of result) {
+    const li = document.createElement('p');
+    const lang_span = document.createElement('span');
+    const result_span = document.createElement('span');
+    li.setAttribute('class', 'translated-result');
+    lang_span.setAttribute('class', 'lang');
+    result_span.setAttribute('class', 'result');
+
+    lang_span.textContent = i.language;
+    result_span.textContent = i.text;
+
+    li.append(lang_span, result_span);
+    div.appendChild(li);
+  }
+
+  return div;
 }
