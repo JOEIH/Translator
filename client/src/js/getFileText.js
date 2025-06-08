61207f192f06a0a13d2e6cffb5a96577229c13d1
JOEIH <joh73459@gmail.com>
2025-06-09 05:36:39 +0900

    ✨ Feat : 추출된 텍스트에서 공백인 열은 제외하고 출력

--- client/src/js/getFileText.js
+++ client/src/js/getFileText.js
@@ -3,16 +3,21 @@ const content = document.querySelector('.content');
 
 // 추출된 텍스트 렌더링
 function getFile() {
-  const extracted_text = localStorage.getItem('fileText');
+  const data = localStorage.getItem('fileInfo');
 
-  if (extracted_text) {
-    const text = JSON.parse(extracted_text).extracted_text.split('\n');
+  if (data) {
+    const parsed = JSON.parse(data);
+    const text = parsed.extracted_text.split('\n');
+    title.textContent = parsed.filename;
 
+    // 공백인 열을 제외하고 출력
     for (let p of text) {
-      content.insertAdjacentHTML(
-        'beforeend',
-        `<p style="width=100%;">${p}</p>`
-      );
+      if (p.trim().length != 0) {
+        content.insertAdjacentHTML(
+          'beforeend',
+          `<p class="extracted" style="width=100%;">${p}</p>`
+        );
+      }
     }
   }
 }
